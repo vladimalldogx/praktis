9c7a258a1f23f1de9dde41b2699f1d05c2e93df3
MEMEMALLEN\Vladimir <shiyocz@gmail.com>
2022-09-07 20:47:14 +0800

    input user data

---
+++ addNew.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add new Data</title>
+</head>
+<body>
+    <form action="query.php" method="POST">
+    <label>Name:</label><br>
+    <input type="text" name="fname">
+    <br>
+    <label>Last:</label><br>
+    <input type="text" name="lastn">
+    <br>
+    <label>Nick:</label><br>
+    <input type="text" name="niname">
+    <br>
+    <button type="submit">Add</button>
+    </form>
+</body>
+</html>
